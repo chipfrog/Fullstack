a5c7bc1c90157c515308772960d4f3009a12be25
Juuso Järvenpää <juuso.kp.jarvenpaa@outlook.com>
2019-10-22 16:12:59 +0300

    Tehtävä 4.15

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -4,6 +4,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
@@ -12,6 +13,7 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
 app.use(cors())
 app.use(bodyParser.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 app.use(middleware.errorHandler)
 
 module.exports = app
