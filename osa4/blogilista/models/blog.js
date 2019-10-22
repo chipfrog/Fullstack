638b12e2b207cd75a8b8dae6cdac8d544519f433
Juuso Järvenpää <juuso.kp.jarvenpaa@outlook.com>
2019-10-22 20:39:19 +0300

    Tehtävä 4.17

--- osa4/blogilista/models/blog.js
+++ osa4/blogilista/models/blog.js
@@ -4,13 +4,18 @@ const blogSchema = new mongoose.Schema({
   title: {type: String, required: true},
   author: String,
   url: {type: String, required: true},
-  likes: Number
+  likes: Number,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
 })
 
 blogSchema.set('toJSON', {
     transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString()
         delete returnedObject._id
+        delete returnedObject.__v
     }
 })
 
--- osa4/blogilista/models/user.js
+++ osa4/blogilista/models/user.js
@@ -9,7 +9,13 @@ const userSchema = mongoose.Schema({
     minlength: 3
   },
   password: String,
-  name: String 
+  name: String,
+  blogs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Blog'
+    }
+  ] 
 })
 userSchema.plugin(uniqueValidator)
 
