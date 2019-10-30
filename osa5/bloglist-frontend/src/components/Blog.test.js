860e246f82affd869fc0756403e8a42d44c560e2
Juuso Järvenpää <juuso.kp.jarvenpaa@outlook.com>
2019-10-30 20:33:40 +0200

    Tehtävä 5.15

---
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, cleanup, fireEvent } from '@testing-library/react'
+import Blog from './Blog'
+
+afterEach(cleanup)
+
+test('at first only title and author are visible', () => {
+  const blog = {
+    title: 'testBlog',
+    author: 'tester',
+    url: 'www.google.com',
+    likes: 10
+  }
+  const component = render(
+    <Blog blog={blog} />
+  )
+  expect(component.container).toHaveTextContent(
+    'testBlog tester'
+  )
+  expect(component.container).not.toHaveTextContent(
+    'likes'
+  )
+})
+
+test('blog information becomes visible after clicking', () => {
+  const user = {
+    name: 'Teppo',
+    username: 'TestMan'
+  }
+  const blog = {
+    title: 'testBlog',
+    author: 'tester',
+    url: 'www.google.com',
+    likes: 10,
+    user: user
+  }
+  const component = render(
+    <Blog blog={blog} user={user} />
+  )
+  expect(component.container).not.toHaveTextContent(
+    '10 likes'
+  )
+  const button = component.getByText('testBlog tester')
+  fireEvent.click(button)
+
+  expect(component.container).toHaveTextContent(
+    '10 likes'
+  )
+  expect(component.container).toHaveTextContent(
+    'www.google.com'
+  )
+})
