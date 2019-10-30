8719b7e159c1c4dee12dabca0335e0d333e4fd07
Juuso Järvenpää <juuso.kp.jarvenpaa@outlook.com>
2019-10-30 19:38:36 +0200

    Tehtävä 5.14

--- osa5/bloglist-frontend/src/components/SimpleBlog.test.js
+++ osa5/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render , cleanup } from '@testing-library/react'
+import { render , cleanup, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import SimpleBlog from './SimpleBlog'
 
@@ -21,3 +21,22 @@ test('renders blog information', () => {
   expect(likes).toHaveTextContent('blog has 5 likes')
 
 })
+
+test('clicking button calls event handler once', async () => {
+  const blog = {
+    title: 'Urheilublogi',
+    author: 'Teppo Testaaja',
+    likes: 5
+  }
+  const mockHandler = jest.fn()
+
+  const { getByText } = render(
+    <SimpleBlog blog={blog} onClick={mockHandler} />
+  )
+  const button = getByText('like')
+  fireEvent.click(button)
+  fireEvent.click(button)
+
+  expect(mockHandler.mock.calls.length).toBe(2)
+})
+
