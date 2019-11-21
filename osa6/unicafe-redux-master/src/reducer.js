36f704716fc8265c3a204902b198de5a3d48f003
Juuso Järvenpää <juuso.kp.jarvenpaa@outlook.com>
2019-11-21 13:51:16 +0200

    Tehtävät 6.1 ja 6.2

---
+++ osa6/unicafe-redux-master/src/reducer.js
@@ -0,0 +1,31 @@
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0
+}
+
+const counterReducer = (state = initialState, action) => {
+  console.log(action)
+  const newState = Object.assign({}, state)
+
+  switch (action.type) {
+    case 'GOOD':
+      newState.good += 1
+      return newState
+    case 'OK':
+      newState.ok += 1
+      return newState
+    case 'BAD':
+      newState.bad += 1
+      return newState
+    case 'ZERO':
+      newState.good = 0
+      newState.ok = 0
+      newState.bad = 0
+      return newState
+    default: return newState
+  }
+  
+}
+
+export default counterReducer
