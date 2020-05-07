96736db2c932442519e4ec2317fffb2b04a896f1
Juuso Järvenpää <juuso.kp.jarvenpaa@outlook.com>
2020-05-07 12:55:42 +0300

    7.2

--- osa7/routed-anecdotes-master/src/App.js
+++ osa7/routed-anecdotes-master/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 import {
   BrowserRouter as Router,
-  Switch,Route, Link
+  Switch, Route, Link, useParams
 } from 'react-router-dom'
 
 const Menu = ({anecdotes, addNew}) => {
@@ -18,6 +18,9 @@ const Menu = ({anecdotes, addNew}) => {
       </div>
 
       <Switch>
+        <Route path="/anecdotes/:id">
+          <Anecdote anecdotes={anecdotes} />
+        </Route>
         <Route path="/about">
           <About />
         </Route>
@@ -33,11 +36,31 @@ const Menu = ({anecdotes, addNew}) => {
   )
 }
 
+const Anecdote = ({ anecdotes }) => {
+  const id = useParams().id
+  console.log(`id: ${id}`)
+  const anecdote = anecdotes.find(n => n.id === id)
+  console.log(`anecdote: ${anecdote}`)
+
+  return (
+  <div>
+    <h2>{anecdote.content} by {anecdote.author}</h2>
+    <p>has { anecdote.votes } votes</p>
+    <p>for more info see <a href={anecdote.info}>{anecdote.info}</a></p>
+  </div>
+  )
+}
+
 const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => 
+        <li key={anecdote.id} >
+          <Link to={`/anecdotes/${anecdote.id}`}>
+            {anecdote.content}
+          </Link>
+        </li>)}
     </ul>
   </div>
 )
