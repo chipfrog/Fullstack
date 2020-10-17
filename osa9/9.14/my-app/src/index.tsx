c486a535981f7f42b3f485fc723653fec8991e33
Juuso Järvenpää <juuso.kp.jarvenpaa@outlook.com>
2020-10-17 22:50:14 +0300

    9.15

--- osa9/9.14/my-app/src/index.tsx
+++ osa9/9.14/my-app/src/index.tsx
@@ -5,22 +5,15 @@ const Header: React.FC<{ name: string }> = ({ name }) => {
   return <h1>{name}</h1>;
 };
 
-interface Element {
-  name: string;
-  exerciseCount: number;
-}
-
-const Content: React.FC<{ list: Element[] }> = ({ list }) => (
+const Content: React.FC<{list: CoursePart[] }> = ({ list }) => (
   <React.Fragment>
-    {list.map(element => (
-      <p key={element.name}>
-        {element.name} {element.exerciseCount}
-      </p>
+    {list.map(part => (
+      <Part key={part.name} props={part}/>
     ))}
   </React.Fragment>
 )
 
-const Total: React.FC<{ list: Element[] }> = ({ list }) => {
+const Total: React.FC<{ list: CoursePart[] }> = ({ list }) => {
   let sum = 0;
   for (let i = 0; i < list.length; i ++) {
     sum += list[i].exerciseCount
@@ -28,21 +21,101 @@ const Total: React.FC<{ list: Element[] }> = ({ list }) => {
   return <div>Number of exercises {sum}</div>
 }
 
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+}
+
+interface CoursePartOne extends CourseCombination {
+  name: "Fundamentals";
+}
+
+interface CoursePartTwo extends CoursePartBase {
+  name: "Using props to pass data";
+  groupProjectCount: number;
+}
+
+interface CoursePartThree extends CourseCombination {
+  name: "Deeper type usage";
+  exerciseSubmissionLink: string;
+}
+
+interface CoursePartFour extends CourseCombination {
+  name: "Random part";
+  difficulty: number;
+}
+
+interface CourseCombination extends CoursePartBase {
+  description: string;
+}
+
+type CoursePart = CoursePartOne | CoursePartTwo | CoursePartThree | CoursePartFour;
+
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled error: ${JSON.stringify(value)}`
+  );
+};
+
+interface PartProps {
+  props: CoursePart
+}
+
+const Part: React.FC<PartProps> = ({ props }) => {
+  switch (props.name) {
+    case 'Fundamentals':
+      return (
+        <div>
+          <p>name: {props.name}, exercises: {props.exerciseCount}, description: {props.description}</p>
+        </div>
+      )
+    case 'Using props to pass data':
+      return (
+        <div>
+          <p>name: {props.name}, exercises: {props.exerciseCount}, group projects: {props.groupProjectCount}</p>
+        </div>
+      )
+    case "Deeper type usage":
+      return (
+        <div>
+          <p>name: {props.name}, exercises: {props.exerciseCount}, description: {props.description}, submission link: {props.exerciseSubmissionLink}</p>
+        </div>
+      )
+    case "Random part":
+      return(
+        <div>
+          <p>name: {props.name}, exercises: {props.exerciseCount}, description: {props.description}, difficulty: {props.difficulty}</p>
+        </div>
+      )  
+    default:
+      return assertNever(props); 
+  }
+}
 
 const App: React.FC = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is an awesome course part"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev"
+    },
+    {
+      name: "Random part",
+      exerciseCount: 100,
+      description: "Very difficult",
+      difficulty: 7
     }
   ];
 
